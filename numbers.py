7b06dea1c0e3e02a98a29f3ea5d7755aa688d736
Malcolm <malcolm.maclure@gmail.com>
2019-02-25 08:45:32 -0500

    comparison

--- numbers.py
+++ numbers.py
@@ -17,3 +17,42 @@ import math
 print(math.floor(2.9))
 print(math.ceil(2.9))
 
+is_hot = True
+is_cold = True
+
+if is_hot:
+    print("It's a hot day.")
+    print("Drink plenty of water.")
+elif is_cold:
+    print("It's a cold day.")
+    print("Wear warm clothes.")
+else:
+    print("It's a lovely day.")
+print("Enjoy your day.")
+
+has_high_income = True
+has_good_credit = True
+has_criminal_record = False
+
+if has_high_income and has_good_credit:
+    print("Eligable for loan")
+
+if has_high_income or has_good_credit:
+    print("Eligable for loan")
+
+if has_good_credit and not has_criminal_record:
+    print("Eligable for loan")
+
+temperature = 35
+
+if temperature == 30:
+    print ("it's 30 degrees")
+
+if temperature != 30:
+    print ("it's not 30 degrees")
+
+if temperature <= 30:
+    print("it's less than 30 degrees")
+
+if temperature >= 30:
+    print("it's more than 30 degrees")
