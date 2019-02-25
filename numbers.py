3fcec7e56d139f3a9c9b7ea9e6f33349e991c903
Malcolm <malcolm.maclure@gmail.com>
2019-02-25 08:31:57 -0500

    basic math

---
+++ numbers.py
@@ -0,0 +1,19 @@
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+X = 10
+X += 3
+print(X)
+
+y = 2.9
+print(round(y))
+print(abs(-2.9))
+
+print("Python3 math module")
+import math
+
+print(math.floor(2.9))
+print(math.ceil(2.9))
+
